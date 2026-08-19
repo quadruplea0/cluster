4ac9ce86a414a3364c06bf07740058217272128c
noob <papakisprits@gmail.com>
2026-08-19 15:16:42 +0300

    fixing refactoring and fix q

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,9 @@ use crossterm::{
     cursor::{Hide, MoveTo, Show},
     event::{poll, read, Event, KeyCode},
     execute,
-    terminal::{Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen, size},
+    terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen,},
 };
-
-use figlet_rs::{Toilet};
-
+use figlet_rs::Toilet;
 use std::{
     io::{stdout, Write},
     time::Duration,
@@ -16,38 +14,43 @@ use time::OffsetDateTime;
 
 fn main() -> std::io::Result<()> {
     let mut stdout = stdout();
-
-    let (cols, _ows) = size()?;
-
+    let (cols, _rows) = size()?;
     execute!(stdout, EnterAlternateScreen, Hide)?;
+    enable_raw_mode()?;
+
     let timeformat = format_description!("[hour]:[minute]:[second]");
     let timeformatdate = format_description!("[year]:[month]:[day]");
-
     let mono9_font = Toilet::mono9().unwrap();
+
     loop {
         execute!(stdout, Clear(ClearType::FromCursorDown), MoveTo(0, 0))?;
-
-        if let Ok(now) = OffsetDateTime::now_local(){
+        if let Ok(now) = OffsetDateTime::now_local() {
             let time = now.format(&timeformat).unwrap();
             let date = now.format(&timeformatdate).unwrap();
             let width = cols as usize;
             let timefiglet = mono9_font.convert(&time).unwrap().to_string();
+
             for line in timefiglet.lines() {
-                println!("{:^width$}", line, width = width);
+                write!(stdout, "{:^width$}\r\n", line, width = width)?;
             }
-            print!("{:^width$}", date, width = width);
+            write!(stdout, "{:^width$}", date, width = width)?;
             stdout.flush()?;
-        } else{
-            println!("failed to find timezone");
+        } else {
+            write!(stdout, "failed to find timezone\r\n")?;
+            stdout.flush()?;
             break;
         }
 
+        if poll(Duration::from_secs(1))? {
             if let Event::Key(event) = read()? {
-                if event.code == KeyCode::Char('q') { break;}
+                if event.code == KeyCode::Char('q') {
+                    break;
+                }
             }
-
+        }
     }
 
+    disable_raw_mode()?;
     execute!(stdout, Show, LeaveAlternateScreen)?;
     Ok(())
 }
